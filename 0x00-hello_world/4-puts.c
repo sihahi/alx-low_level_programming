01cbc88ed64a77377655cf0392b8e93f50979636
sihahi <siham.hida27@gmail.com>
2023-04-24 03:24:20 -0700

    task 4 hello world C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
- * main - A program that prints a line with puts function 
- * Return: 0 (Success)
+ * main - A program that prints a line with puts function.
+ * Return: 0 (Success).
  */
 int main(void)
 {
