eaa1fff2e43d238d31e1f60179c9457faac480a8
sihahi <siham.hida27@gmail.com>
2023-05-03 08:25:38 -0700

    task 9 pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcpy - function that copies the string pointed
+ * @dest: char type string
+ * @src: char type string
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = scr[i];
+	} while (scr[i] != '\0');
+
+	return (dest);
+}
