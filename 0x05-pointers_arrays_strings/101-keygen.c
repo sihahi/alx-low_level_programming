63be33f56e3162106cee0c278f3b42792ddc8333
sihahi <siham.hida27@gmail.com>
2023-05-03 15:54:36 -0700

    task 11 pointers

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -  program that generates random valid passwords
+ * for the program 101-crackme
+ * Return: zero
+ */
+
+int main(void)
+{
+	int sum;
+	char c;
+
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 120;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
