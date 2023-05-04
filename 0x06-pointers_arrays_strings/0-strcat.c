ca3820434f0c71ba770c518ae4698480bb603ceb
sihahi <siham.hida27@gmail.com>
2023-05-04 15:16:24 -0700

    task 0 More pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ *
+ * @dest: Destination pointer input
+ * @src: Source pointer input
+ * Return:  pointer to the resulting string @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int c, c1;
+
+	c = 0;
+	while (dest[c])
+		c++;
+
+	for (c1 = 0; src[c1]; c1++)
+		dest[c++] = src[c2];
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the caracter c to stdout
+ * @c: the character to print
+ *
+ * Return: 1 (success)
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
