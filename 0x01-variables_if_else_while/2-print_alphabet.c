16073e37ce7d198c9e6d8977a91e13f02c59f104
sihahi <siham.hida27@gmail.com>
2023-05-08 04:38:00 -0700

    task 2 variables C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio>
+
+/**
+ * main - entry point
+ *
+ * description: print all alphabet letters
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
