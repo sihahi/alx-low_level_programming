8799b84b019e2d26c9678d7b5216140bbec862a2
sihahi <siham.hida27@gmail.com>
2023-05-11 15:31:27 -0700

    task 5 recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _sqrt_recursion - function that returns the natural square root of a number
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,6 +11,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-int square(int n, int sqr);
 
 #endif /*MAIN_H*/
