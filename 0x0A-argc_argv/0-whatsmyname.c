73c96098f11d45b1d69a49ac8659adf5df10ca07
sihahi <siham.hida27@gmail.com>
2023-05-14 12:59:39 -0700

    task 0 argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the programm name
+ * @argc: counter
+ * @argv: value/list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+
+}
