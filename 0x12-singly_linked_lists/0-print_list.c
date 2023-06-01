4aaf1b7aa087bd4573ab9ec01d7b979f003e94a8
sihahi <siham.hida27@gmail.com>
2023-06-01 13:06:58 -0700

    ah

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -25,13 +25,13 @@ int _strlen(char *s)
 
 size_t print_list(const list_t *h)
 {
-	size_t n = 0;
+	size_t i = 0;
 
 	while (h)
 	{
 		printf("[%d] %s\n", _strlen(h->str). h->str ? h->str : "(nil)");
 		h = h->next;
-		n++;
+		i++;
 	}
-	return (n);
+	return (i);
 }
--- 0x12-singly_linked_lists/_putchar.c
+++ 0x12-singly_linked_lists/_putchar.c
@@ -1,9 +1,11 @@
 #include "lists.h"
-
+#include <unistd.h>
 /**
- * _putchar - function to print
- * @c: character
- * Return: 1 on success
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
 int _putchar(char c)
