7585f35badd1b87c8a681e2a3fc2f58498792359
sihahi <siham.hida27@gmail.com>
2023-06-01 12:54:01 -0700

    task 0 single links

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * _strlen - the length of string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * print_list - prints linked list
+ * @h: pointer
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str). h->str ? h->str : "(nil)");
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
