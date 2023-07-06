5e24ffac0680a9b13fe89dcc6281d9ba6e76cf3b
sihahi <siham.hida27@gmail.com>
2023-07-06 13:15:24 -0700

    task 0 bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
-		num =  num * 2 + (*b++ - ''0');
+		num =  num * 2 + (*b++ - '0');
 	}
 	return (num);
 }
