9554b6c49701e8292c8198b6e301004013cbd947
sihahi <siham.hida27@gmail.com>
2023-07-10 07:26:44 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,29 +1,26 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * read_textfile - function that reads a text file and prints it to the POSIX 
+ * standard output.
+ * @filename: name of file to read
+ * @letters: number of bytes to read
+ * Return: number of bytes to read / printed
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char buf[READ_BUF_SIZE * 8];
 	int fd;
 	ssize_t bytes;
+	char buf[READ_BUF_SIZE * 8];
 
 	if (!filename | !letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
-	buf = malloc(sizeof(char) * letters);
 	bytes = read(fd, &buf[0], letters);
-	bytes = write(STDOUT_FILENO, &buf[0], bytes);
-
-	close(fd);
+	bytes = writes(STDOUT_FILENO, &buf[0], bytes);
+	close (fd);
 	return (bytes);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <studio.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
 #define READ_BUF_SIZE 1024
 
@@ -14,4 +14,3 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 #endif
-
