0b8c34d35da67ece3f7bf94341dffad097cf9fe4
sihahi <siham.hida27@gmail.com>
2023-07-10 07:38:40 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytes;
 	char buf[READ_BUF_SIZE * 8];
 
-	if (!filename | !letters)
+	if (!filename || !letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
