af2372b35546997c095c2904e39eee0cc6d78c5c
sihahi <siham.hida27@gmail.com>
2023-07-29 08:22:45 -0700

    yhva

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,6 +11,15 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_entry(Elf64_Ehdr h);
+void print_type(Elf64_Ehdr h);
+void print_abiversion(Elf64-Ehdr h);
+void print_osabi_more(Elf64_Ehdr h);
+void print_osabi(Elf64_Ehdr h);
+void print_version(Elf64_Ehdr h);
+void print_data(Elf64_Ehdr h);
+void print_class(Elf64_Ehdr h);
+int main(int ac, char **av);
 
 #endif
 
